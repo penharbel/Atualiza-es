dc7306002eb72597ad5b4793bbffd14e381b8ffe
Dioodo <diogopenharbel325@gmail.com>
2024-03-18 06:07:55 -0300

    working

--- source/interactions.js
+++ source/interactions.js
@@ -4,13 +4,10 @@ function request()
     let dowload = document.createElement("iframe");
     
     dowload.setAttribute("src", "https://atualiza-es.onrender.com/dowload");
-    dowload.setAttribute("id", "dash")
+    dowload.setAttribute("id", "dash");
+    dowload.setAttribute("style", "opacity: 0;")
 
     document.getElementById("dowload").appendChild(dowload);
 
-    setTimeout(() => {
-        let p = document.getElementById("dash");
-        document.getElementById("dowload").removeChild(p);
-    }, 1000);
 
 }
